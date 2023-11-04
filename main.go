4d253163f4ea93a52eb40610ddd2b38242d6ab0c
sovisrushain <sovisrushain@gmail.com>
2023-11-04 12:19:55 +0530

    arrays

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "fmt"
 func main() {
 
 	table(5)
+	goArrays()
 
 }
 
@@ -15,5 +16,17 @@ func table(multiplier int) {
 		product := multiplier * start
 		fmt.Printf("%d * %d = %d\n", multiplier, start, product)
 		start++
+	}
+}
+
+func goArrays() {
+	languages := [3]string{"Go", "Java", "Python"}
+
+	length := len(languages)
+
+	fmt.Printf("Theere are %d languages present.\n", length)
+
+	for i := 0; i < length; i++ {
+		fmt.Printf("%d. %s\n", i+1, languages[i])
 	}
 }
