b451c53c047e0ad6a7078f4bbcf254037b22a862
sovisrushain <sovisrushain@gmail.com>
2023-11-04 15:42:53 +0530

    map

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ func main() {
 	table(5)
 	goArrays()
 	goSlice()
+	goMap()
 
 }
 
 func table(multiplier int) {
+
 	start := 1
 
 	for start <= 10 {
@@ -18,9 +20,11 @@ func table(multiplier int) {
 		fmt.Printf("%d * %d = %d\n", multiplier, start, product)
 		start++
 	}
+
 }
 
 func goArrays() {
+
 	languages := [3]string{"Go", "Java", "Python"}
 
 	length := len(languages)
@@ -30,9 +34,11 @@ func goArrays() {
 	for i := 0; i < length; i++ {
 		fmt.Printf("%d. %s\n", i+1, languages[i])
 	}
+
 }
 
 func goSlice() {
+
 	listOfNumbers := [5]int{1, 2, 3, 4, 5}
 	evenNumbers := []int{}
 	oddNumbers := []int{}
@@ -48,4 +54,27 @@ func goSlice() {
 	fmt.Printf("There are %d even numbers and %d odd numbers are present\n",
 		len(evenNumbers),
 		len(oddNumbers))
+
+}
+
+func goMap() {
+
+	marks := map[string]int{
+		"Math":    98,
+		"Science": 95,
+		"History": 90,
+	}
+
+	marks["Geography"] = 88
+
+	fmt.Println(marks)
+
+	delete(marks, "History")
+
+	fmt.Println(marks)
+
+	for sub, score := range marks {
+		fmt.Printf("%s: %d\n", sub, score)
+	}
+
 }
